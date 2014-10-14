1e82151edde7400cdd7001aedc555f92cafab033
Wesley Crick <cric5890@mylaurier.ca>
2014-10-14 08:56:53 -0400

    Cleaned
    Code is now fully commented and organized a little nicer.

--- Image Proc/FilterImage.java
+++ Image Proc/FilterImage.java
@@ -7,21 +7,17 @@ import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
 
-public class FilterImage
-{
-	
-	/*int[] filter = {
-					-1,-1,-1,
-					-1, 8,-1,
-					-1,-1,-1
-					};*/
-
-//	int[] filter = {1,1,1,1,1,1,1,1,1};		//Low pass filter
+public class FilterImage {
 	
 	JFrame imageFrame = new JFrame("Images");
     JFrame editorFrame = new JFrame("Editor");
     BufferedImage changed_image = null;
     BufferedImage image = null;
+    
+    JTextField matrix_size_text_fieled = null;
+    JPanel matrix_panel = new JPanel();
+    JTextField resize_text_field = null;
+    JPanel image_panel = new JPanel();
     
     
   public static void main(String[] args) throws Exception
@@ -53,11 +49,10 @@ public class FilterImage
     });
   }
   
-  JTextField matrix_size_text_fieled = null;
-  JPanel matrix_panel = new JPanel();
-  JTextField resize_text_field = null;
-  JPanel image_panel = new JPanel();
-  
+
+  /**
+   * editorFrameLayout	-	Creates a layout for the editor frame
+   */
   private void editorFrameLayout() {
 	  JLabel matrix_size_label = new JLabel("Matrix Size:");
 	  matrix_size_text_fieled = new JTextField();
@@ -86,19 +81,17 @@ public class FilterImage
 	  editorFrame.getContentPane().add(matrix_panel, BorderLayout.CENTER);
 	  editorFrame.getContentPane().add(resize_outer_panel, BorderLayout.SOUTH);
   }
-  
+  /**
+   * imageFrameLayout 	-	creates a layout for the frame with the images
+   * @param filename	-	the image filename
+   */
   private void imageFrameLayout(String filename) {
 	  
 		
-      try
-      {
+      try {
         image = ImageIO.read(new File(filename));
-        changed_image = convertToGrayScale(image);
-		//changed_image = filterImage(changed_image);
-		  
-      }
-      catch (Exception e)
-      {
+        changed_image = convertToGrayScale(image); 
+      } catch (Exception e) {
         e.printStackTrace();
         System.exit(1);
       }
@@ -108,82 +101,70 @@ public class FilterImage
 	  ImageIcon changed_icon = new ImageIcon(changed_image);
 	  JLabel j_label = new JLabel();
       j_label.setIcon(changed_icon);
-		
-      //imageFrame.getContentPane().add(jLabel, BorderLayout.CENTER);
-      //imageFrame.getContentPane().add(j_label, BorderLayout.SOUTH);
       
       image_panel = new JPanel( new BorderLayout() );
-      
       image_panel.add(jLabel, BorderLayout.NORTH);
       image_panel.add(j_label, BorderLayout.SOUTH);     
       
       imageFrame.add(image_panel);
   }
   
-  private BufferedImage changeColour( BufferedImage first ) {
-	BufferedImage old = deepCopy(first);
-	int[] new_rgb = old.getRGB(0,0, old.getWidth(), old.getHeight(), null, 0, old.getWidth());
-	for ( int i = 0; i < new_rgb.length; i++ ) {
-		System.out.println(new_rgb[i]);
-		if ( new_rgb[i] == -16777216 ) {
-			new_rgb[i] = -8388608;
-		}
-	}
-	old.setRGB(0,0,old.getWidth(), old.getHeight(),new_rgb, 0, old.getWidth());
-	return old;
-  }
-  
+  /**
+   * filterImage	-	applies a filter to the old image
+   * @param first	-	the old image
+   * @param filter	-	the filter to be applied, can be low or high pass
+   * @return	The new image with a filter applied to it
+   */
   private BufferedImage filterImage( BufferedImage first, int[] filter ) {
 	  BufferedImage old = deepCopy(first);
 	  int[] old_rgb = old.getRGB(0,0, old.getWidth(), old.getHeight(), null, 0, old.getWidth());
 	  int[] new_rgb = old.getRGB(0,0, old.getWidth(), old.getHeight(), null, 0, old.getWidth());
 	  
 	  for ( int i = 0; i < new_rgb.length; i++ ) {
-		 // if ( i % old.getWidth() != 0 && i % old.getHeight() != 0 ) {
-			  new_rgb[i] = (int)filterImageAux(old_rgb, i, filter, old.getWidth(), old.getHeight());
-		 // }
+		  new_rgb[i] = (int)filterImageAux(old_rgb, i, filter, old.getWidth(), old.getHeight());
 	  }
 	  
 	  old.setRGB(0,0,old.getWidth(), old.getHeight(),new_rgb, 0, old.getWidth());
 	  return old;
   }
-  
+  /**
+   * filterImageAux - 	given the starting pixel it will apply a filter
+   * 
+   * @param rgb		-	the color values	
+   * @param center	-	the center pixel
+   * @param filter	-	the filter to apply
+   * @param img_width	-	the old images width
+   * @param img_height	-	the old images height
+   * @return	An averaged value for the pixel
+   */
   private double filterImageAux(int[] rgb, int center, int[] filter, int img_width, int img_height) {;
-	int sum = 0;  
-	int sum_red = 0;
-	  int sum_green = 0;
-	  int sum_blue = 0;
+  	  int sum = 0;  
 	  int x_filter = 0;
 	  int y_filter = 0;
 	  			// move up															move left
 	  int start = center - ( ( (int)Math.sqrt(filter.length) -1 ) / 2 )*img_width - ( (int)Math.sqrt(filter.length) -1 ) / 2;
+	  //check if with in the bounds of the image
 	  if ( start < 0 ) {
 		  return rgb[center];
 	  } else if ( Math.sqrt(filter.length) + start + Math.sqrt(filter.length)*img_width > img_width*img_height  ) {
 		  return rgb[center];
 	  }
 	  
-	  //System.out.println( Math.sqrt(filter.length) + start + " " + Math.sqrt(filter.length)*img_width + " " + img_width*img_height);
 	  
 	  for ( int x = start; x < Math.sqrt(filter.length) + start; x++ ) {
 		  y_filter = 0;
 		  for ( int y = 0; y < Math.sqrt(filter.length)*img_width; y+= img_width ) {
-
 			  Color c = new Color(rgb[x+y]);
-			  sum += c.getRed() * filter[x_filter+y_filter];
-
-			  //System.out.println(c.getRed());
-			  
+			  sum += c.getRed() * filter[x_filter+y_filter];		  
 			  y_filter+=Math.sqrt(filter.length);
 		  }
 		  x_filter++;
 	  }
 	 
-	  //System.out.println("New Value: " + (double)sum / (double)arraySum(filter));
+	  //checks for different possible sums
 	  int temp = sum;
 	  if ( arraySum(filter) != 0 ) { 
 		temp = (int)((double)sum / (double)arraySum(filter)); 
-		//int  temp = (int)((double)sum / (double)filter.length); 
 	  }
 	  if ( temp < 0 ) {
 		  temp = 0;
@@ -193,10 +174,13 @@ public class FilterImage
 		  temp = 255;
 	  }
 	  
-	  //System.out.println(temp);
 	  return new Color(temp,temp,temp).getRGB();
   }
-  
+  /**
+   * arraySum - sums a given array
+   * @param array	The array to sum
+   * @return	An int of the summed array
+   */
   private int arraySum(int[] array ) {
 	  int sum = 0;
 	  for ( int i = 0; i < array.length; i++) {
@@ -204,35 +188,67 @@ public class FilterImage
 	  }
 	  return sum;
   }
-  
+  /**
+   * convertToGrayScale - converts the image to grayscale
+   * @param first	The old image
+   * @return	A new image with grayscale color
+   */
   private BufferedImage convertToGrayScale(BufferedImage first ) {
 	  BufferedImage old = deepCopy(first);
 	  int[] new_rgb = old.getRGB(0,0, old.getWidth(), old.getHeight(), null, 0, old.getWidth());
 	  
 	  for ( int i = 0; i < new_rgb.length; i++ ) {
-		 // if ( i % old.getWidth() != 0 && i % old.getHeight() != 0 ) {
 		  Color c = new Color(new_rgb[i]);
 		  int red = (int)(c.getRed() * 0.299);
 		  int green =(int)(c.getGreen() * 0.587);
 		  int blue = (int)(c.getBlue() *0.114);
 		  c = new Color(red+green+blue, red+green+blue,red+green+blue);
 		  new_rgb[i] = c.getRGB();
-		 // }
 	  }
 	  
 	  old.setRGB(0,0,old.getWidth(), old.getHeight(),new_rgb, 0, old.getWidth());
 	  return old;
   }
-  
+  	/**
+  	 * deepCopy - takes an image and does a deep copy
+  	 *  
+  	 * @param bi	The image to copy
+  	 * @return	The deep copied image
+  	 */
 	private BufferedImage deepCopy(BufferedImage bi) {
 		 ColorModel cm = bi.getColorModel();
 		 boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
 		 WritableRaster raster = bi.copyData(null);
 		 return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
 	}
+	/**
+	 * redrawImageFrame - when an event happens to change the new image
+	 */
+	public void redrawImageFrame() {
+		ImageIcon imageIcon = new ImageIcon(image);
+	    JLabel jLabel = new JLabel();
+		jLabel.setIcon(imageIcon);
+		ImageIcon changed_icon = new ImageIcon(changed_image);
+		JLabel j_label = new JLabel();
+	    j_label.setIcon(changed_icon);
+	    
+	    image_panel.removeAll();
+	    
+	    image_panel.add(jLabel, BorderLayout.NORTH);
+	    image_panel.add(j_label, BorderLayout.SOUTH);   
+		
+		imageFrame.pack();
+		imageFrame.revalidate();
+		imageFrame.repaint();
+	}
 	
 //=================================================================================================
 //	Events
+	
+	/**
+	 * 	MatrixSizeListener - A listener that will act when the matrix button is clicked.
+	 * 		It will create a square matrix depending on what number was entered.
+	 */
 	public class MatrixSizeListener implements ActionListener {
 
 	@Override
@@ -268,11 +284,14 @@ public class FilterImage
 		
 	}
 	
+	/**
+	 * 	ApplyToPicture - is a listener for the apply matrix to image button.
+	 * 		It will take the matrix entered and apply it to the image.
+	 */
 	public class ApplyToPicture implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent ae) {
-			// TODO Auto-generated method stub
 			try {
 				JPanel temp_panel = (JPanel)matrix_panel.getComponent(0);	//list of text fields
 				
@@ -283,7 +302,6 @@ public class FilterImage
 				for ( int x = 0; x < count; x++ ) {
 					for ( int y = 0; y < count; y++ ) {
 						JTextField temp_text_field = (JTextField)temp_panel.getComponent(x*count + y);
-						//System.out.println( temp_text_field.getText() );
 						filter[x*count + y] = Integer.parseInt(temp_text_field.getText().trim());
 					}
 				}
@@ -293,9 +311,6 @@ public class FilterImage
 				changed_image = filterImage(image, filter);
 				redrawImageFrame();
 				
-				File outputfile = new File("saved.png");
-				ImageIO.write(changed_image, "png", outputfile);
-				
 			}catch(Exception e) {
 				System.out.println("Error on matrix input: " + e.getMessage() );
 			}
@@ -303,40 +318,26 @@ public class FilterImage
 		
 	}
 	
-	public void redrawImageFrame() {
-		ImageIcon imageIcon = new ImageIcon(image);
-	    JLabel jLabel = new JLabel();
-		jLabel.setIcon(imageIcon);
-		ImageIcon changed_icon = new ImageIcon(changed_image);
-		JLabel j_label = new JLabel();
-	    j_label.setIcon(changed_icon);
-	    
-	    image_panel.removeAll();
-	    
-	    image_panel.add(jLabel, BorderLayout.NORTH);
-	    image_panel.add(j_label, BorderLayout.SOUTH);   
-		
-		imageFrame.pack();
-		imageFrame.revalidate();
-		imageFrame.repaint();
-	}
-	
+
+	/**
+	 * 	ResizePicture - is a listener for the resize button.
+	 * 		It will either shrink or grow the image depending on if
+	 * 		the number entered is less than 1, it will shrink,
+	 * 		if greater than 1 it will grow.
+	 */
 	public class ResizePicture implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent ae) {
-			// TODO Auto-generated method stub
 			try {
 				double resize = Double.parseDouble( resize_text_field.getText() );
-				//double resize = 0.5;
-				
-				//BufferedImage old = deepCopy(image);
+
 				int[] old_rgb = image.getRGB(0,0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
 				
-				int width = (int)(image.getWidth() * resize);		//3		if max_width = 6 and max_height = 6 at 0.5
-				int height = (int)(image.getHeight() * resize);		//3
-				int width_division = image.getWidth()/width;		//2
-				int height_division = image.getHeight()/height;		//2
+				int width = (int)(image.getWidth() * resize);		
+				int height = (int)(image.getHeight() * resize);		
+				int width_division = image.getWidth()/width;		
+				int height_division = image.getHeight()/height;		
 				
 				int[] new_rgb = new int[width*height];
 
@@ -356,43 +357,61 @@ public class FilterImage
 					}
 				}
 				changed_image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-				//System.out.println("Created new image");
 				changed_image.setRGB(0, 0, width, height, new_rgb, 0, width );
-				//System.out.println("set rgb");
 				redrawImageFrame();
 			} catch (Exception e) {
 				System.out.println("Error on Resize input: " + e.getMessage() );
 			}
 		}
-		
+		/**
+		 * 	growImage 		- 	a method to take a portion of the image and expand it
+		 * 
+		 * @param rgb		- 	the colour values
+		 * @param resize	- 	the amount the image will be resized
+		 * @param pos_x		-	start position in the x
+		 * @param pos_y		-	start position in the y
+		 * @param width		-	width of the new image
+		 * @param height	-	height of the new image
+		 * @param new_rgb	-	the new image
+		 * @param image_width	-	the old image width
+		 * @return
+		 */
 		private int[] growImage(int[] rgb, double resize, int pos_x, int pos_y, int width, int height, int[] new_rgb, int image_width ) {
 			Color c = new Color(rgb[pos_x + pos_y*image_width]);
 			int[] rgb_copy = new_rgb;
 			for ( int x = pos_x*(int)resize; x < pos_x*(int)resize + (int)resize; x++ ) {
 				for ( int y = pos_y*width*(int)resize; y < pos_y*width*(int)resize + width*(int)resize; y+= width ) {
 					rgb_copy[x+y] = c.getRGB(); 
-					//System.out.println(x + ", " + y);
 				}
 			}
 			
 			return rgb_copy;
 		}
-		
+		/**
+		 * 	averageShrink 	- 	a method to shrink the image with an averaged sum
+		 * 
+		 * @param rgb		- 	the colour values
+		 * @param resize	- 	the amount the image will be resized
+		 * @param pos_x		-	start position in the x
+		 * @param pos_y		-	start position in the y
+		 * @param width		-	width of the new image
+		 * @param height	-	height of the new image
+		 * @param new_rgb	-	the new image
+		 * @param image_width	-	the old image width
+		 * @return
+		 */
 		private int averageShrink(int[] rgb, int width_div, int height_div, int pos_x, int pos_y, int image_width, int image_height) {
 			int sum = 0;
-			//System.out.println(width_div + " " + height_div );
 			for ( int x = pos_x*width_div; x < pos_x*width_div + width_div; x++ ) {
 				for ( int y = pos_y*image_width*height_div; y < pos_y*image_width*height_div + image_width*height_div; y+= image_width ) {
-					//sum += rgb[x+y];
 					Color c = new Color(rgb[x+y]);
 					sum += c.getRed();
-					//System.out.println(x + "," + y + " colour: " + c.getRed() + " sum: " + sum);
 				}
 			}
-			//System.out.println("Sum: " + sum + " Color Code: " + sum/(width_div*height_div) );
+			
 			int temp = sum/(width_div*height_div);
 			Color c = new Color(temp,temp,temp);
-			//System.out.println(c.getRGB() );
+
 			return c.getRGB();
 		}
 		
